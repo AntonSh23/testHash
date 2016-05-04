3dde16e312fe09cf3cadbafee4f6bcc32f9fc221
AntonSh23 <ash52131@gmail.com>
2016-05-04 19:55:45 +0300

    Added files via upload

---
+++ base.sql
@@ -0,0 +1,37 @@
+-- MySQL dump 10.13  Distrib 5.6.24, for Win64 (x86_64)
+--
+-- Host: localhost    Database: hash_storage
+-- ------------------------------------------------------
+-- Server version	5.6.27-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping data for table `unit_data`
+--
+
+LOCK TABLES `unit_data` WRITE;
+/*!40000 ALTER TABLE `unit_data` DISABLE KEYS */;
+INSERT INTO `unit_data` VALUES (2,'data/2016-05-04_16-33-17/','2016-05-04_16-33-17',NULL),(3,'data/2016-05-04_16-33-22/','2016-05-04_16-33-22',NULL),(4,'data/2016-05-04_16-56-00/','2016-05-04_16-56-00',NULL),(5,'data/2016-05-04_16-56-20/','2016-05-04_16-56-20',NULL),(6,'data/2016-05-04_17-07-35/','2016-05-04_17-07-35',NULL),(7,'data/2016-05-04_17-07-38/','2016-05-04_17-07-38',NULL),(8,'data/2016-05-04_17-13-29/','2016-05-04_17-13-29',NULL),(9,'data/2016-05-04_17-14-04/','2016-05-04_17-14-04',NULL),(10,'data/2016-05-04_17-14-28/','2016-05-04_17-14-28',NULL),(11,'data/2016-05-04_17-14-36/','2016-05-04_17-14-36',NULL),(12,'data/2016-05-04_17-14-37/','2016-05-04_17-14-37',NULL),(13,'data/2016-05-04_17-15-08/','2016-05-04_17-15-08',NULL),(14,'data/2016-05-04_17-15-40/','2016-05-04_17-15-40',NULL),(15,'data/2016-05-04_17-16-00/','2016-05-04_17-16-00',NULL),(16,'data/2016-05-04_17-16-06/','2016-05-04_17-16-06',NULL),(17,'data/2016-05-04_17-32-34/','2016-05-04_17-32-34','preloader.gif'),(18,'data/2016-05-04_17-33-39/','2016-05-04_17-33-39','preloader.gif'),(19,'data/2016-05-04_17-35-08/','2016-05-04_17-35-08','preloader.gif'),(20,'data/2016-05-04_17-42-39/','2016-05-04_17-42-39','preloader.gif'),(21,'data/2016-05-04_17-44-38/','2016-05-04_17-44-38','preloader.gif'),(22,'data/2016-05-04_17-44-50/','2016-05-04_17-44-50','preloader.gif'),(23,'data/2016-05-04_17-45-03/','2016-05-04_17-45-03','preloader.gif'),(24,'data/2016-05-04_17-45-03/','2016-05-04_17-45-03','preloader.gif'),(25,'data/2016-05-04_17-45-04/','2016-05-04_17-45-04','preloader.gif'),(26,'data/2016-05-04_17-45-10/','2016-05-04_17-45-10','preloader.gif'),(27,'data/2016-05-04_17-45-10/','2016-05-04_17-45-10','preloader.gif'),(28,'data/2016-05-04_17-46-48/','2016-05-04_17-46-48','preloader.gif'),(29,'data/2016-05-04_17-47-05/','2016-05-04_17-47-05','preloader.gif'),(30,'data/2016-05-04_17-47-13/','2016-05-04_17-47-13','preloader.gif'),(31,'data/2016-05-04_17-47-49/','2016-05-04_17-47-49','preloader.gif'),(32,'data/2016-05-04_17-48-37/','2016-05-04_17-48-37','preloader.gif'),(33,'data/2016-05-04_17-53-01/','2016-05-04_17-53-01','preloader.gif'),(34,'data/2016-05-04_17-53-17/','2016-05-04_17-53-17','preloader.gif'),(35,'data/2016-05-04_17-55-32/','2016-05-04_17-55-32','preloader.gif'),(36,'data/2016-05-04_18-06-37/','2016-05-04_18-06-37','preloader.gif'),(37,'data/2016-05-04_18-10-33/','2016-05-04_18-10-33','preloader.gif'),(38,'data/2016-05-04_18-12-18/','2016-05-04_18-12-18','preloader.gif'),(39,'data/2016-05-04_18-15-19/','2016-05-04_18-15-19','preloader.gif'),(40,'data/2016-05-04_18-16-10/','2016-05-04_18-16-10','preloader.gif'),(41,'data/2016-05-04_18-16-41/','2016-05-04_18-16-41','preloader.gif'),(42,'data/2016-05-04_19-14-33/','2016-05-04_19-14-33','ExportFromBase.xls'),(43,'data/2016-05-04_19-18-04/','2016-05-04_19-18-04','ExportFromBase.xls'),(44,'data/2016-05-04_19-20-14/','2016-05-04_19-20-14','ExportFromBase.xls'),(45,'data/2016-05-04_19-20-36/','2016-05-04_19-20-36','ExportFromBase.xls'),(46,'data/2016-05-04_19-21-00/','2016-05-04_19-21-00','ExportFromBase.xls'),(47,'data/2016-05-04_19-23-19/','2016-05-04_19-23-19','ExportFromBase.xls'),(48,'data/2016-05-04_19-24-02/','2016-05-04_19-24-02','ExportFromBase.xls'),(49,'data/2016-05-04_19-24-12/','2016-05-04_19-24-12','ExportFromBase.xls'),(50,'data/2016-05-04_19-24-36/','2016-05-04_19-24-36','ExportFromBase.xls'),(51,'data/2016-05-04_19-24-41/','2016-05-04_19-24-41','ExportFromBase.xls'),(52,'data/2016-05-04_19-24-45/','2016-05-04_19-24-45','ExportFromBase.xls'),(53,'data/2016-05-04_19-25-02/','2016-05-04_19-25-02','ExportFromBase.xls'),(54,'data/2016-05-04_19-25-10/','2016-05-04_19-25-10','ExportFromBase.xls'),(55,'data/2016-05-04_19-25-10/','2016-05-04_19-25-10','ExportFromBase.xls'),(56,'data/2016-05-04_19-25-34/','2016-05-04_19-25-34','ExportFromBase.xls'),(57,'data/2016-05-04_19-26-33/','2016-05-04_19-26-33','ExportFromBase.xls'),(58,'data/2016-05-04_19-27-03/','2016-05-04_19-27-03','ExportFromBase.xls'),(59,'data/2016-05-04_19-27-09/','2016-05-04_19-27-09','ExportFromBase.xls'),(60,'data/2016-05-04_19-27-23/','2016-05-04_19-27-23','ExportFromBase.xls'),(61,'data/2016-05-04_19-27-40/','2016-05-04_19-27-40','ExportFromBase.xls'),(62,'data/2016-05-04_19-27-51/','2016-05-04_19-27-51','ExportFromBase.xls'),(63,'data/2016-05-04_19-27-59/','2016-05-04_19-27-59','ExportFromBase.xls'),(64,'data/2016-05-04_19-28-13/','2016-05-04_19-28-13','ExportFromBase.xls'),(65,'data/2016-05-04_19-28-32/','2016-05-04_19-28-32','ExportFromBase.xls'),(66,'data/2016-05-04_19-28-52/','2016-05-04_19-28-52','ExportFromBase.xls'),(67,'data/2016-05-04_19-30-49/','2016-05-04_19-30-49','data-2016-05-04_16-33-17-null'),(68,'data/2016-05-04_19-40-28/','2016-05-04_19-40-28','data-2016-05-04_16-31-57-preloader.gif'),(69,'data/2016-05-04_19-53-56/','2016-05-04_19-53-56','data-2016-05-04_16-31-57-preloader.gif'),(70,'data/2016-05-04_19-54-25/','2016-05-04_19-54-25','data-2016-05-04_16-31-57-preloader.gif');
+/*!40000 ALTER TABLE `unit_data` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-05-04 19:55:30
